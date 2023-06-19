53cc100dab4eac8851d1db14e86c08f534d71c2c
Jordan Kisiel <jordan.kisiel@gmail.com>
2023-06-19 19:46:59 -0400

    Initial commit and setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        screens: {
+            sm: "500px",
+            md: "750px",
+            lg: "1000px",
+            short: { raw: "(max-height:800px)" },
+        },
+        extend: {
+            colors: {
+                neutral: {
+                    100: "#ffffff",
+                    900: "#000000",
+                },
+                purple: {
+                    400: "#7945FF",
+                    500: "#5C2DD5",
+                },
+                red: {
+                    300: "#FD6687",
+                },
+                yellow: {
+                    300: "#FFCE57",
+                },
+            },
+            boxShadow: {
+                "2xl": "0px 10px 0px #000000",
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
