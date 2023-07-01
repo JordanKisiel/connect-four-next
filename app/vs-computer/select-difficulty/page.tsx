ff5561ae4d73b4a64facacf2362270c169fec68c
Jordan Kisiel <jordan.kisiel@gmail.com>
2023-07-01 01:39:53 -0400

    mobile styling issue fixes

--- app/vs-computer/select-difficulty/page.tsx
+++ app/vs-computer/select-difficulty/page.tsx
@@ -7,48 +7,52 @@ import Button from "@/app/components/Button"
 export default function Home() {
     return (
         <>
-            <Image className="mb-16" src={logo} alt="logo" />
-            <div className="flex w-full flex-1 flex-col space-y-6">
-                <h2 className="text-center text-4xl font-bold uppercase text-neutral-100">
+            
+            <div className="flex w-full flex-col items-center">
+                <Image className="mb-16" src={logo} alt="logo" />
+                <h2 className="text-center text-3xl font-bold uppercase text-neutral-100  mb-10">
                     Select Difficulty
                 </h2>
-                <MenuButton
-                    bgColor="bg-purple-500"
-                    textColor="text-neutral-100"
-                    textAlign="text-center"
-                    padding="p-3"
-                    path="/vs-computer/game/easy"
-                >
-                    Easy
-                </MenuButton>
-                <MenuButton
-                    bgColor="bg-purple-500"
-                    textColor="text-neutral-100"
-                    textAlign="text-center"
-                    padding="p-3"
-                    path="/vs-computer/game/medium"
-                >
-                    Medium
-                </MenuButton>
-                <MenuButton
-                    bgColor="bg-purple-500"
-                    textColor="text-neutral-100"
-                    textAlign="text-center"
-                    padding="p-3"
-                    path="/vs-computer/game/hard"
-                >
-                    Hard
-                </MenuButton>
+                <div className="w-full flex flex-col space-y-6 mb-10 px-2">
+                    <MenuButton
+                        bgColor="bg-purple-500"
+                        textColor="text-neutral-100"
+                        textAlign="text-center"
+                        padding="p-3"
+                        path="/vs-computer/game/easy"
+                    >
+                        Easy
+                    </MenuButton>
+                    <MenuButton
+                        bgColor="bg-purple-500"
+                        textColor="text-neutral-100"
+                        textAlign="text-center"
+                        padding="p-3"
+                        path="/vs-computer/game/medium"
+                    >
+                        Medium
+                    </MenuButton>
+                    <MenuButton
+                        bgColor="bg-purple-500"
+                        textColor="text-neutral-100"
+                        textAlign="text-center"
+                        padding="p-3"
+                        path="/vs-computer/game/hard"
+                    >
+                        Hard
+                    </MenuButton>
+                </div>
+                <Link href="/">
+                    <Button
+                        textColor="text-neutral-100"
+                        bgColor="bg-purple-500"
+                        paddingX="px-6"
+                    >
+                        Back to Main Menu
+                    </Button>
+                </Link>
             </div>
-            <Link href="/">
-                <Button
-                    textColor="text-neutral-100"
-                    bgColor="bg-purple-500"
-                    paddingX="px-6"
-                >
-                    Back to Main Menu
-                </Button>
-            </Link>
+            
         </>
     )
 }
