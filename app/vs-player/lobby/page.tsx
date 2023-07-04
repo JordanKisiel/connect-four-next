7afc2d7d3a73e8082e6b257c1fd9dc214614019d
Jordan Kisiel <jordan.kisiel@gmail.com>
2023-07-04 00:17:28 -0400

    added back to menu button

--- app/vs-player/lobby/page.tsx
+++ app/vs-player/lobby/page.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import Room from '@/app/components/Room'
+import Button from '@/app/components/Button'
 import logo from '@/public/logo.svg'
 
 export default function Lobby() {
@@ -14,13 +16,24 @@ export default function Lobby() {
         )
     })
 
-  return (
-    <div className="mx-auto flex h-[100vh] w-[90%] flex-col items-center justify-center p-4 space-y-6 sm:w-[90%] lg:w-[60%]">
-        <Image className="mb-16" src={logo} alt="logo" />
-        <h2 className="text-center text-3xl font-bold uppercase text-neutral-100  mb-10">
-            Select Room
-        </h2>
-        {rooms}
-    </div>
-  )
+    return (
+        <div className="mx-auto flex h-[100vh] w-[90%] flex-col items-center justify-center p-4 sm:w-[90%] lg:w-[60%]">
+            <Image className="mb-16" src={logo} alt="logo" />
+            <h2 className="text-center text-3xl font-bold uppercase text-neutral-100  mb-10">
+                Select Room
+            </h2>
+            <div className="space-y-6 w-full">
+            {rooms}
+            </div>
+            <Link className="block mt-16" href="/">
+                <Button
+                    textColor="text-neutral-100"
+                    bgColor="bg-purple-500"
+                    paddingX="px-6"
+                >
+                    Back to Main Menu
+                </Button>
+            </Link>
+        </div>
+    )
 }
