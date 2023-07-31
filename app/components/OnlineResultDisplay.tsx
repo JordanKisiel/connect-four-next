da2d2ff7225899d763f5084c8e46565d34f87734
Jordan Kisiel <jordan.kisiel@gmail.com>
2023-07-31 18:28:27 -0400

    Added OnlineResultDisplay component

---
+++ app/components/OnlineResultDisplay.tsx
@@ -0,0 +1,81 @@
+import Button from "./Button"
+
+type Props = {
+    isWinner: boolean
+    isPlayerWinner: boolean
+    isBoardFull: boolean
+    isPlayer1: boolean
+    handler: Function
+}
+
+export default function ResultDisplay({
+    isWinner,
+    isPlayerWinner,
+    isBoardFull,
+    isPlayer1,
+    handler,
+}: Props) {
+    const player = "You"
+    let resultText = ""
+
+    //cases to account for:
+    // player1 wins game - player2 loses game
+    // player2 wins game - player1 loses game
+    // draw
+    // player1 wins by forfeit
+    // player2 wins by forfeit
+
+    if (isWinner) {
+        if (isPlayerWinner) {
+            resultText = "Win"
+        } else {
+            resultText = "Lose"
+        }
+    } else if (isBoardFull) {
+        resultText = "Draw"
+    } else {
+        resultText = "Win By Forfeit"
+    }
+
+    return (
+        <div
+            className="
+            absolute
+            top-[65%]
+            flex
+            w-5/6
+            flex-col
+            items-center
+            rounded-3xl
+            border-[3px]
+            border-neutral-900
+            bg-neutral-100
+            bg-[center_right_1rem]
+            bg-no-repeat
+            px-5
+            py-5
+            text-neutral-900
+            shadow-2xl
+            md:top-[47%]
+            md:w-[70%]
+            lg:top-[67%]
+            lg:w-[75%]
+            "
+        >
+            <span className="text-center text-base font-bold uppercase md:text-2xl">
+                {player}
+            </span>
+            <span className="mb-1.5 text-center text-6xl font-bold uppercase md:mb-5 md:text-7xl">
+                {resultText}
+            </span>
+            <Button
+                bgColor="bg-purple-500"
+                textColor="text-neutral-100"
+                paddingX="px-7"
+                handler={() => handler()}
+            >
+                Play Again
+            </Button>
+        </div>
+    )
+}
