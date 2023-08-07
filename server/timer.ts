303e9c1277ebd79899a9b677e4a73be04a425e4a
Jordan Kisiel <jordan.kisiel@gmail.com>
2023-08-07 16:21:32 -0400

    Updated Timer class

--- server/timer.ts
+++ server/timer.ts
@@ -1,24 +1,30 @@
 export class Timer {
     startTime: number
     remainingTime: number
+    callbackFunction: Function
+    interval: NodeJS.Timeout | undefined
 
-    constructor(startTime: number) {
+    constructor(startTime: number, callbackFunction: Function) {
         this.startTime = startTime
         this.remainingTime = startTime
+        this.callbackFunction = callbackFunction
+        this.interval = undefined
     }
 
-    start(callbackFunction: Function) {
-        const interval = setInterval(() => {
+    start() {
+        this.interval = setInterval(() => {
             if (this.remainingTime > 0) {
                 this.remainingTime -= 1
             } else {
-                callbackFunction()
-                clearInterval(interval)
+                this.callbackFunction()
+                clearInterval(this.interval)
             }
         }, 1000)
     }
 
     reset() {
         this.remainingTime = this.startTime
+        clearInterval(this.interval)
+        this.start()
     }
 }
