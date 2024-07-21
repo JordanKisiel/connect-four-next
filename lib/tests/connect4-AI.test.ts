3e164d29a63d4baa4b21cc0d0c089cb1dd0df1fd
Jordan Kisiel <jordan.kisiel@gmail.com>
2024-07-21 15:28:37 -0400

    Update: replaced magic numbers with constants

--- lib/tests/connect4-AI.test.ts
+++ lib/tests/connect4-AI.test.ts
@@ -1,6 +1,22 @@
 import { getChildPositions, chooseIndex } from "@/lib/connect4-AI"
-import { l } from "vite/dist/node/types.d-aGj9QkWt"
 import { test, expect } from "vitest"
+
+//seed for testing random functionality
+const SEED = "1234"
+
+//difficulty velocities
+const EASY_DIFF = 24
+const MEDIUM_DIFF = 18
+const HARD_DIFF = 15
+
+//board dimensions
+const NUM_ROWS = 6
+const NUM_COLS = 7
+
+//index weights
+const ONE_INDEX = [1]
+const TWO_INDICES = [0.5, 0.5]
+const THREE_INDICES = [0.5, 0.4, 0.1]
 
 test("getChildPositions: all cols open", () => {
     const board = [
@@ -131,97 +147,115 @@ test("getChildPositions: 1 col open, multiple spaces", () => {
 //using "1234" as optional seed
 
 test("chooseIndex: only 1 col open, fully open, easy", () => {
-    expect(chooseIndex([1], 1 / 6, 24, "1234")).toBe(0)
+    expect(chooseIndex(ONE_INDEX, 1 / NUM_ROWS, EASY_DIFF, SEED)).toBe(0)
 })
 
 test("chooseIndex: only 1 col open, fully open, medium", () => {
-    expect(chooseIndex([1], 1 / 6, 18, "1234")).toBe(0)
+    expect(chooseIndex(ONE_INDEX, 1 / NUM_ROWS, MEDIUM_DIFF, SEED)).toBe(0)
 })
 
 test("chooseIndex: only 1 col open, fully open, hard", () => {
-    expect(chooseIndex([1], 1 / 6, 15, "1234")).toBe(0)
+    expect(chooseIndex(ONE_INDEX, 1 / NUM_ROWS, HARD_DIFF, SEED)).toBe(0)
 })
 
 test("chooseIndex: only 1 col open, 1 space open, easy", () => {
-    expect(chooseIndex([1], 1 / 1, 24, "1234")).toBe(0)
+    expect(chooseIndex(ONE_INDEX, 1 / 1, EASY_DIFF, SEED)).toBe(0)
 })
 
 test("chooseIndex: only 1 col open, 1 space open, medium", () => {
-    expect(chooseIndex([1], 1 / 1, 18, "1234")).toBe(0)
+    expect(chooseIndex(ONE_INDEX, 1 / 1, MEDIUM_DIFF, SEED)).toBe(0)
 })
 
 test("chooseIndex: only 1 col open, 1 space open, hard", () => {
-    expect(chooseIndex([1], 1 / 1, 15, "1234")).toBe(0)
+    expect(chooseIndex(ONE_INDEX, 1 / 1, HARD_DIFF, SEED)).toBe(0)
 })
 
 test("chooseIndex: only 2 col open, fully open, easy", () => {
-    expect(chooseIndex([0.5, 0.5], 1 / 12, 24, "1234")).toBe(1)
+    expect(chooseIndex(TWO_INDICES, (1 / 2) * NUM_ROWS, EASY_DIFF, SEED)).toBe(
+        1
+    )
 })
 
 test("chooseIndex: only 2 col open, fully open, medium", () => {
-    expect(chooseIndex([0.5, 0.5], 1 / 12, 18, "1234")).toBe(0)
+    expect(
+        chooseIndex(TWO_INDICES, (1 / 2) * NUM_ROWS, MEDIUM_DIFF, SEED)
+    ).toBe(0)
 })
 
 test("chooseIndex: only 2 col open, fully open, hard", () => {
-    expect(chooseIndex([0.5, 0.5], 1 / 12, 15, "1234")).toBe(0)
+    expect(chooseIndex(TWO_INDICES, (1 / 2) * NUM_ROWS, HARD_DIFF, SEED)).toBe(
+        0
+    )
 })
 
 test("chooseIndex: only 2 col open, 1 space open, easy", () => {
-    expect(chooseIndex([0.5, 0.5], 1 / 2, 24, "1234")).toBe(1)
+    expect(chooseIndex(TWO_INDICES, 1 / 2, EASY_DIFF, SEED)).toBe(1)
 })
 
 test("chooseIndex: only 2 col open, 1 space open, medium", () => {
-    expect(chooseIndex([0.5, 0.5], 1 / 2, 18, "1234")).toBe(1)
+    expect(chooseIndex(TWO_INDICES, 1 / 2, MEDIUM_DIFF, SEED)).toBe(1)
 })
 
 test("chooseIndex: only 2 col open, 1 space open, hard", () => {
-    expect(chooseIndex([0.5, 0.5], 1 / 2, 15, "1234")).toBe(1)
+    expect(chooseIndex(TWO_INDICES, 1 / 2, HARD_DIFF, SEED)).toBe(1)
 })
 
 test("chooseIndex: 3 col open, fully open, easy", () => {
-    expect(chooseIndex([0.5, 0.4, 0.1], 1 / 18, 24, "1234")).toBe(0)
+    expect(
+        chooseIndex(THREE_INDICES, (1 / 3) * NUM_ROWS, EASY_DIFF, SEED)
+    ).toBe(0)
 })
 
 test("chooseIndex: 3 col open, fully open, medium", () => {
-    expect(chooseIndex([0.5, 0.4, 0.1], 1 / 18, 18, "1234")).toBe(0)
+    expect(
+        chooseIndex(THREE_INDICES, (1 / 3) * NUM_ROWS, MEDIUM_DIFF, SEED)
+    ).toBe(0)
 })
 
 test("chooseIndex: 3 col open, fully open, hard", () => {
-    expect(chooseIndex([0.5, 0.4, 0.1], 1 / 18, 15, "1234")).toBe(0)
+    expect(
+        chooseIndex(THREE_INDICES, (1 / 3) * NUM_ROWS, HARD_DIFF, SEED)
+    ).toBe(0)
 })
 
 test("chooseIndex: 3 col open, 1 space open, easy", () => {
-    expect(chooseIndex([0.5, 0.4, 0.1], 1 / 3, 24, "1234")).toBe(2)
+    expect(chooseIndex(THREE_INDICES, 1 / 3, EASY_DIFF, SEED)).toBe(2)
 })
 
 test("chooseIndex: 3 col open, 1 space open, medium", () => {
-    expect(chooseIndex([0.5, 0.4, 0.1], 1 / 3, 18, "1234")).toBe(2)
+    expect(chooseIndex(THREE_INDICES, 1 / 3, MEDIUM_DIFF, SEED)).toBe(2)
 })
 
 test("chooseIndex: 3 col open, 1 space open, hard", () => {
-    expect(chooseIndex([0.5, 0.4, 0.1], 1 / 3, 15, "1234")).toBe(2)
+    expect(chooseIndex(THREE_INDICES, 1 / 3, HARD_DIFF, SEED)).toBe(2)
 })
 
 test("chooseIndex: all col open, 1 space open, easy", () => {
-    expect(chooseIndex([0.5, 0.4, 0.1], 1 / 7, 24, "1234")).toBe(2)
+    expect(chooseIndex(THREE_INDICES, 1 / NUM_COLS, EASY_DIFF, SEED)).toBe(2)
 })
 
 test("chooseIndex: all col open, 1 space open, medium", () => {
-    expect(chooseIndex([0.5, 0.4, 0.1], 1 / 7, 18, "1234")).toBe(1)
+    expect(chooseIndex(THREE_INDICES, 1 / NUM_COLS, MEDIUM_DIFF, SEED)).toBe(1)
 })
 
 test("chooseIndex: all col open, 1 space open, hard", () => {
-    expect(chooseIndex([0.5, 0.4, 0.1], 1 / 7, 15, "1234")).toBe(1)
+    expect(chooseIndex(THREE_INDICES, 1 / NUM_COLS, HARD_DIFF, SEED)).toBe(1)
 })
 
 test("chooseIndex: all col open, all spaces open, easy", () => {
-    expect(chooseIndex([0.5, 0.4, 0.1], 1 / 42, 24, "1234")).toBe(0)
+    expect(
+        chooseIndex(THREE_INDICES, (1 / NUM_ROWS) * NUM_COLS, EASY_DIFF, SEED)
+    ).toBe(0)
 })
 
 test("chooseIndex: all col open, all spaces open, medium", () => {
-    expect(chooseIndex([0.5, 0.4, 0.1], 1 / 42, 18, "1234")).toBe(0)
+    expect(
+        chooseIndex(THREE_INDICES, (1 / NUM_ROWS) * NUM_COLS, MEDIUM_DIFF, SEED)
+    ).toBe(0)
 })
 
 test("chooseIndex: all col open, all spaces open, hard", () => {
-    expect(chooseIndex([0.5, 0.4, 0.1], 1 / 42, 15, "1234")).toBe(0)
+    expect(
+        chooseIndex(THREE_INDICES, (1 / NUM_ROWS) * NUM_COLS, HARD_DIFF, SEED)
+    ).toBe(0)
 })
